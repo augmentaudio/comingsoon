7f3c4f9ff5850237c8f1fccd49439ca5d6243356
augment <augmentaudio@gmail.com>
2018-09-22 23:01:42 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,7 +31,7 @@
     /* ==========================================================================
     countdown timer
     ========================================================================== */
-     jQuery('#clock').countdown('2018/11/11',function(event){
+     jQuery('#clock').countdown('2018/10/21',function(event){
       var $this=jQuery(this).html(event.strftime(''
       +'<div class="time-entry days"><span>%-D</span> Days</div> '
       +'<div class="time-entry hours"><span>%H</span> Hours</div> '
